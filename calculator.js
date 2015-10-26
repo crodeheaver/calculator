e6bce84aa861a209c94e5edb4ab0dd732d68dace
crodeheaver <colbyrodeheaver@hotmail.com>
2015-10-26 02:33:36 +0000

    changed a couple of buttons

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ var specialButtons = {
     clear: "Clear"
 }
 
-var buttonList = ["(", ")", "^", specialButtons.backspace, "1", "2", "3", specialButtons.clear, "4", "5", "6", "-", "7", "8", "9", "*", ".", "0", "=", "+"];
+var buttonList = ["^", specialButtons.clear, specialButtons.backspace,"%", "1", "2", "3","/", "4", "5", "6", "-", "7", "8", "9", "*", ".", "0", "=", "+"];
 
 function create(tagName, props, eventprops, event) {
     var element = document.createElement(tagName);
